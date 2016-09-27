34a233683a330b0f0301befc1c084b19d5206a09
S.Hall <simon.p.hall@accenture.com>
2016-09-27 11:02:14 +0200

    adding new method for getting session username

--- PHP/Session.php
+++ PHP/Session.php
@@ -13,12 +13,13 @@ class Session {
         error_reporting(E_ERROR);
 
         // Get data from client POST
-        $data = json_decode(file_get_contents("php://input"));
+        $raw_data = file_get_contents("php://input");
+        $data = json_decode($raw_data, false);
+        
         $username = mysql_real_escape_string($data->username);
         $password = mysql_real_escape_string($data->password);
         $sessionkey = $this->GUID();
-        // Verify log-in
-
+        
         // See if credentials match
         $User = new User();
         if($User->checkLogin() != 'OK')
@@ -42,7 +43,7 @@ class Session {
         mysql_select_db($Settings->db_name, $conn);
 
         // query to insert workItem
-        $query = "INSERT INTO " . $this->table_name . " (username, alive, sessionkey, time_start, time_lastseen) VALUES ('" . $username . "', 1, '". $sessionkey ."', NOW(), NOW())";
+        $query = "INSERT INTO " . $this->table_name . " (username, alive, sessionkey, time_start, time_lastseen) VALUES ('" . $data->username . "', 1, '". $sessionkey ."', NOW(), NOW())";
 
         // execute query
         if (mysql_query($query))
@@ -134,6 +135,65 @@ class Session {
         else
         {
             return "fail";
+        }
+    }
+    
+    function getUsername()
+    {
+        error_reporting(E_ERROR);
+
+        // Get data from client POST
+        $raw_data = file_get_contents("php://input");
+        $data = json_decode($raw_data, false);
+        $sessionkey = $data->sessionkey;
+
+        if($this->verifySession() != "OK")
+        {
+            return "ERRORSESSION";
+        }
+        // Create connection
+        $Settings = new Settings();
+        $conn = mysql_connect($Settings->host, $Settings->username, $Settings->password);
+
+        // Check connection
+        if (!$conn)
+        {
+            die('Could not connect: ' . mysql_error());
+            return "SQLERROR";
+        }
+
+        mysql_select_db($Settings->db_name, $conn);
+        
+        // query to insert workItem
+        $query = "SELECT username FROM " . $this->table_name . " WHERE sessionkey ='". $sessionkey . "'";
+
+        // execute query
+        $result = mysql_query($query);
+        if (!$result)
+        {
+            //echo "SQL ERROR could not get tasks in Workitem()";
+            mysql_close($conn);
+            return "ERRORSQL";
+        }
+        else
+        {
+            //create an array
+            $emparray = array();
+            while($row = mysql_fetch_assoc($result))
+            {
+                $emparray[] = $row;
+            }
+            //close the db connection
+            mysql_close($conn);
+            
+            if(count($emparray) > 0)
+            {
+                return $emparray[0]['username'];
+            }
+            else
+            {
+                return "ERRORSESSION";
+            }
         }
     }
     
